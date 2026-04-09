078778f54d8a7ba8e37d351aed04b7e0baa132a6
Ratnesh Kumar <ratnesh134@gmail.com>
2026-04-09 18:52:41 +0530

    Keyword Search implement

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,26 @@
+import argparse
+from lib.keyword_search import search_command
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            # print the search query here
+            print(f"Searching for : {args.query}")
+            results = search_command(args.query,5)
+            
+            for i, result in enumerate(results):
+                print(f"{i} {result['title']}")
+        case _:
+            parser.print_help()
+
+if __name__ == "__main__":
+    main()
