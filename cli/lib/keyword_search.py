47a63d10b1420847e9813f803c44f8ef10ba678d
Ratnesh Kumar <ratnesh134@gmail.com>
2026-04-09 18:52:03 +0530

    none return fix

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -2,10 +2,10 @@ from lib.search_utils import load_movies
 
 def search_command(query, n_results):
     movies = load_movies()
-
+    res = []
     for movie in movies:
-        res = []
-        if query in movies['title']:
+        
+        if query in movie['title']:
             res.append(movie)
         if len(res) == n_results:
             break
