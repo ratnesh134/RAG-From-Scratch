02403f3e89ba660a6edca39eb5d224c1a7db8b16
Ratnesh Kumar <ratnesh134@gmail.com>
2026-04-10 13:34:06 +0530

    removing stopwords clean_text

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,4 +1,4 @@
-from lib.search_utils import load_movies
+from lib.search_utils import load_movies, load_stopwords
 import string
 
 def clean_text(text):
@@ -10,9 +10,25 @@ def clean_text(text):
 # Here tokenization is at word level
 def tokenize_text(text):
     text = clean_text(text)
-    tokens = [tok for tok in text.split() if tok]
-    return tokens
+    
+    # loading the stopwords
+    stopwords = load_stopwords() 
+    
+    res = []
 
+    # Function to remove the stopwords
+    def _filter(tok):
+        if tok and tok not in stopwords:
+            return True
+        return False
+    for tok in text.split():
+        if _filter(tok):
+            res.append(tok)
+
+    return res
+
+
+# Function to match the tokenized words
 def has_matching_token(query_tokens, movie_tokens):
     for query_tok in query_tokens:
         for movie_tok in movie_tokens:
