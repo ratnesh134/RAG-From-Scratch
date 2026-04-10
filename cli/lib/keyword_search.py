f57d0ad517d51d03503d258351eba1048772cf10
Ratnesh Kumar <ratnesh134@gmail.com>
2026-04-10 12:07:36 +0530

    clean text function

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,11 +1,20 @@
 from lib.search_utils import load_movies
+import string
+
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("","",string.punctuation))
+
+    return text
+
 
 def search_command(query, n_results):
     movies = load_movies()
     res = []
     for movie in movies:
         
-        if query.lower() in movie['title'].lower():
+        query = clean_text(query)
+        if query in clean_text(movie['title']):
             res.append(movie)
         if len(res) == n_results:
             break
