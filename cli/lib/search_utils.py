2ba1b4fa295f89739a049cfc31c5e15dbf6fdc4e
Ratnesh Kumar <ratnesh134@gmail.com>
2026-04-10 13:33:06 +0530

    load stopwords function

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -2,12 +2,18 @@ import json
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_PATH = PROJECT_ROOT/'data'/'movies.json'
+DATA_PATH_MOVIES = PROJECT_ROOT/'data'/'movies.json'
+DATA_PATH_STOPWORDS = PROJECT_ROOT/'data'/'stopwords.txt'
 
 
 def load_movies() -> list[dict]:
-    with open(DATA_PATH, 'r') as f:
+    with open(DATA_PATH_MOVIES, 'r') as f:
         data = json.load(f)
     
     return data['movies']
 
+def load_stopwords():
+    with open(DATA_PATH_STOPWORDS,"r") as f:
+        data = f.read().splitlines()
+    
+    return data
