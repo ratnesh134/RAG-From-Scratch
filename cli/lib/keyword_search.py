e2fa7656fa1f081728bea6e3c982c5be4c87fed9
Ratnesh Kumar <ratnesh134@gmail.com>
2026-04-15 21:17:26 +0530

    tfidf calculation

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -51,11 +51,10 @@ class InvertedIndex:
         return math.log((doc_count + 1) / (term_match_doc_count + 1))
 
     def get_tfidf(self,doc_id,term):
-        idx = InvertedIndex()
-        idx.load()
-        tf_idf = idx.get_tfidf(doc_id,term)
-        print(f"TF-IDF score of '{term} in document '{doc_id}' : {tf_idf:.2f}")
+        tf = self.get_tf(doc_id,term)
+        idf = self.get_idf(term)
 
+        return tf*idf
 
     def build(self):
         movies = load_movies()
